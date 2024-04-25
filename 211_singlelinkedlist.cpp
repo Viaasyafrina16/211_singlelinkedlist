77f87edf3313e4585fbc5e7fc9e82ac416b64c77
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-25 15:26:16 +0700

    membuat bool listEmpty

--- 211_singlelinkedlist.cpp
+++ 211_singlelinkedlist.cpp
@@ -83,3 +83,10 @@ bool deleteNode(int nim) {
         START = current->;
         return true;
 }
+bool listEmpty() {
+    if (START == NULL)
+        return true;
+    else
+        return false;
+}
+
